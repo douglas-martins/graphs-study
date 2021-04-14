edcc2c8e8868794378e9a990b82e8e400cdfb10a
Douglas Martins <douglasfabiamartins@hotmail.com>
2021-04-14 20:10:59 -0300

    Ajustado grafo

--- src/components/Graph/ForceGraph.tsx
+++ src/components/Graph/ForceGraph.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { ForceGraph3D } from "react-force-graph";
 
-import { useModal } from '@components/Modal/customHooks/useModal'
 
 import { useWindowDimensions } from "@customHooks/useWindowDimensions";
-import { useGraph } from "@customHooks/useGraph";
 import { useStoreActions, useStoreState } from '../../store/storeHooks';
 
 export type GraphDataNode = { id: string, name: string, value: number };
@@ -15,18 +13,47 @@ export type GraphData = {
 };
 
 const ForceGraph = (): JSX.Element => {
-    const { show, toggle, currentModal, changeCurrentModal } = useModal();
     const { height } = useWindowDimensions();
-    const { graphData, updateGraphData } = useGraph();
-
     const graph = useStoreState((state) => state.graph);
-    const createNewGraph = useStoreActions((actions) => actions.createNewGraph);
+    const [graphData, setGraphData] = useState({
+        nodes: new Array<GraphDataNode>(),
+        links: new Array<GraphDataLink>(),
+    })
 
+    useEffect(() => {
+        console.log(graph);
+        if (graph.adjacencyList.length > 0) {
+            parseGraph();
+        }
+    }, [graph.adjacencyList]);
+
+    const parseGraph = (): void => {
+        const newGraphData: GraphData = {
+            links: new Array<GraphDataLink>(),
+            nodes: new Array<GraphDataNode>(),
+        };
+
+        for (const vertex of graph.adjacencyList) {
+            newGraphData.nodes.push({
+                id: vertex.name,
+                name: `${vertex.name}-name`,
+                value: 1,
+            });
+
+            for (const edge of vertex.edges) {
+                newGraphData.links.push({
+                    source: vertex.name,
+                    target: edge.name,
+                });
+            }
+        }
+        setGraphData(newGraphData);
+    }
 
     console.log(graphData)
     return (
         <>
-            <ForceGraph3D graphData={graphData} height={height - 54} />
+            <ForceGraph3D graphData={graphData} height={height - 61} />
         </>
     );
 };
